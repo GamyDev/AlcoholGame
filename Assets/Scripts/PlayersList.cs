70734de8dc7ded3015c0b96a41efcdc0e1f459d5
GamyDev <102431424+GamyDev@users.noreply.github.com>
2022-12-29 00:03:17 +0300

    Fix ScrollBar

--- Assets/Scripts/PlayersList.cs
+++ Assets/Scripts/PlayersList.cs
@@ -1,23 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayersList : MonoBehaviour
 {
     [SerializeField] private GameObject[] _playersList;
     [SerializeField] private int _countPlayers;
     [SerializeField] private GameObject _addPlayersObject;
+    [SerializeField] private Scrollbar _scrollbar;
+    [SerializeField] private GameObject _content;
+    [SerializeField] private Transform _startPosition;
+    [SerializeField] private float _screenHeight;
+    [SerializeField] private float _distance;
 
 
     public void AddPlayers()
     {
         _countPlayers++;
+        ChangePosition();
     }
 
-     
-    
+    private void Start()
+    {
+        _content.transform.position = new Vector3(_startPosition.transform.position.x, _startPosition.transform.position.y, _startPosition.transform.position.z);
 
-    void Update()
+        _screenHeight = Screen.height;
+
+        if (_screenHeight >= 2960)
+        {
+            _distance = -5.697f;
+        }
+
+        if (_screenHeight >= 2732 && _screenHeight < 2960)
+        {
+            _distance = 0.55f;
+        }
+
+        if (_screenHeight >= 2560 && _screenHeight < 2732)
+        {
+            _distance = 0.7175f;
+        }
+
+        if (_screenHeight >= 2388 && _screenHeight < 2560)
+        {
+            _distance = 0.792f;
+        }
+
+        if (_screenHeight >= 2160 && _screenHeight < 2388)
+        {
+            _distance = 0.67f;
+        }
+
+        if (_screenHeight >= 1920 && _screenHeight < 2160)
+        {
+            _distance = 0.53f;
+        }
+
+    }
+
+   public void ChangePosition()
     {
         if (_countPlayers ==1)
         {
@@ -37,6 +79,7 @@ public class PlayersList : MonoBehaviour
         if(_countPlayers == 4)
         {
             _playersList[3].SetActive(true);
+            _scrollbar.value = _distance;
         }
 
         if(_countPlayers == 5)
@@ -47,6 +90,7 @@ public class PlayersList : MonoBehaviour
         if(_countPlayers == 6)
         {
             _playersList[5].SetActive(true);
+            _scrollbar.value = 0;
         }
 
         if(_countPlayers == 7)

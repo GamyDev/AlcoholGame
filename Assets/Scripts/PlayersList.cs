87613bebd1236cd6ae8d6387287e35f6e4518a05
GamyDev <102431424+GamyDev@users.noreply.github.com>
2023-01-16 21:47:21 +0300

    Add New Start Screen

--- Assets/Scripts/PlayersList.cs
+++ Assets/Scripts/PlayersList.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class PlayersList : MonoBehaviour
 {
     [SerializeField] private GameObject[] _playersList;
-    [SerializeField] private int _countPlayers;
+     private int _countPlayers = 0;
     [SerializeField] private GameObject _addPlayersObject;
     [SerializeField] private Scrollbar _scrollbar;
     [SerializeField] private GameObject _content;
-    [SerializeField] private Transform _startPosition;
+    [SerializeField] private GameObject _moveObject;
+    [SerializeField] private GameObject _topTextObject;
+    [SerializeField] private GameObject _topBlueObject;
+    [SerializeField] private Transform[] _positionContent;
+    [SerializeField] private Transform[] _positionMoveObject;
     [SerializeField] private float _screenHeight;
-    [SerializeField] private float _distance;
+    [SerializeField] private float _distanceScroll;
+    [SerializeField] private ScrollRect _scrollRect;
+
+
+    // [SerializeField] private GameObject _distanceScroll;
 
 
     public void AddPlayers()
@@ -23,43 +31,54 @@ public class PlayersList : MonoBehaviour
 
     private void Start()
     {
-        _content.transform.position = new Vector3(_startPosition.transform.position.x, _startPosition.transform.position.y, _startPosition.transform.position.z);
+        _content.transform.position = new Vector3(_positionContent[0].transform.position.x, _positionContent[0].transform.position.y, _positionContent[0].transform.position.z);
 
         _screenHeight = Screen.height;
 
-        if (_screenHeight >= 2960)
+        if (_screenHeight >= 2532 && _screenHeight < 2960)
         {
-            _distance = -5.697f;
+            _distanceScroll = 0.9126536f;
         }
 
-        if (_screenHeight >= 2732 && _screenHeight < 2960)
+        if (_screenHeight >= 2436 && _screenHeight < 2532)
         {
-            _distance = 0.55f;
+            _distanceScroll = 0.9027101f;
         }
 
-        if (_screenHeight >= 2560 && _screenHeight < 2732)
+        if (_screenHeight >= 2340 && _screenHeight < 2436)
         {
-            _distance = 0.7175f;
+            _distanceScroll = 0.9086636f; //
         }
 
-        if (_screenHeight >= 2388 && _screenHeight < 2560)
+        if (_screenHeight >= 2160 && _screenHeight < 2340)
         {
-            _distance = 0.792f;
-        }
-
-        if (_screenHeight >= 2160 && _screenHeight < 2388)
-        {
-            _distance = 0.67f;
+            _distanceScroll = 0.5328956f; //
         }
 
         if (_screenHeight >= 1920 && _screenHeight < 2160)
         {
-            _distance = 0.53f;
+            _distanceScroll = 0.6948383f; //
+        }
+
+        if (_screenHeight >= 1792 && _screenHeight < 1920)
+        {
+            _distanceScroll = 0.8960785f; //
+        }
+
+        if (_screenHeight >= 1334 && _screenHeight < 1792)
+        {
+            _distanceScroll = 0.6730404f; //
+        }
+
+        if (_screenHeight >= 1136 && _screenHeight < 1334)
+        {
+            _distanceScroll = 0.6748586f; //
         }
 
     }
 
-   public void ChangePosition()
+
+    public void ChangePosition()
     {
         if (_countPlayers ==1)
         {
@@ -69,6 +88,8 @@ public class PlayersList : MonoBehaviour
         if(_countPlayers == 2)
         {
             _playersList[1].SetActive(true);
+            _content.transform.position = new Vector3(_positionContent[1].transform.position.x, _positionContent[1].transform.position.y, _positionContent[1].transform.position.z);
+            _moveObject.transform.position = new Vector3(_positionMoveObject[0].transform.position.x, _positionMoveObject[0].transform.position.y, _positionMoveObject[0].transform.position.z);
         }
 
         if(_countPlayers == 3)
@@ -79,7 +100,13 @@ public class PlayersList : MonoBehaviour
         if(_countPlayers == 4)
         {
             _playersList[3].SetActive(true);
-            _scrollbar.value = _distance;
+            _content.transform.position = new Vector3(_positionContent[2].transform.position.x, _positionContent[2].transform.position.y, _positionContent[2].transform.position.z);
+            _moveObject.transform.position = new Vector3(_positionMoveObject[1].transform.position.x, _positionMoveObject[1].transform.position.y, _positionMoveObject[1].transform.position.z);
+
+            _topTextObject.SetActive(false);
+            _topBlueObject.SetActive(true);
+            _scrollRect.enabled = true;
+            _scrollbar.value = _distanceScroll;
         }
 
         if(_countPlayers == 5)
@@ -103,5 +130,7 @@ public class PlayersList : MonoBehaviour
             _playersList[7].SetActive(true);
             _addPlayersObject.SetActive(false);
         }
+
+       
     }
 }
---
+++ Assets/Scripts/TextMeshOutline.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextMeshOutline : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private Color32 color;
+
+    void Start()
+    {
+        textMeshPro.outlineWidth = 0.2f;
+        textMeshPro.outlineColor = color;
+    }
+}

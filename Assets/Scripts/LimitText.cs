e21159e0472477733ec5beaf8add6ec1f785f3db
GamyDev <102431424+GamyDev@users.noreply.github.com>
2023-02-14 13:27:41 +0300

    Fix Animator and Add ButtonBack for Select Deck Game Panel

--- Assets/Scripts/LimitText.cs
+++ Assets/Scripts/LimitText.cs
@@ -7,15 +7,25 @@ public class LimitText : MonoBehaviour
 {
     public int maxLength;
 
+    private void OnEnable()
+    {
+        LocalizationManager.OnLanguageChange += LocalizationManager_OnLanguageChange;
+    }
+
+    private void OnDisable()
+    {
+        LocalizationManager.OnLanguageChange -= LocalizationManager_OnLanguageChange;
+    }
+
     private void Start()
     {
-        Truncate();
-        LocalizationManager.OnLanguageChange += LocalizationManager_OnLanguageChange;
+        Invoke("Truncate", 0.1f);
     }
 
     private void LocalizationManager_OnLanguageChange()
     {
         Truncate();
+        Invoke("Truncate", 0.1f);
     }
 
     public void Truncate()
